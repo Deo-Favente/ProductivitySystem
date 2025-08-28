c129b8196d603cbadcfba965669815b9f045f94a
Deo_Favente <56390262+Deo-Favente@users.noreply.github.com>
2025-08-28 18:13:06 +0200

    changement api target

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  server: { proxy: { "/api": { target: "http://localhost:3001", changeOrigin: true } } }
+  server: {
+    proxy: {
+      "/api": {
+        target: process.env.VITE_API_TARGET || "http://127.0.0.1:3001",
+        changeOrigin: true
+      }
+    }
+  }
 })
