0dda8932348e035a29b8ef2a89ebe9fb9cbfa507
Deo_Favente <56390262+Deo-Favente@users.noreply.github.com>
2025-08-29 03:47:55 +0200

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
+  build: {
+    target: 'es5'   // force un code vieux compatible Safari 9
+  },
+  plugins: [
+    legacy({
+      targets: ['iOS >= 9', 'Safari >= 9'],
+      additionalLegacyPolyfills: [
+        'regenerator-runtime/runtime',
+        'whatwg-fetch',
+        'core-js/features/url',
+        'core-js/features/url-search-params'
+      ]
+    }),
+    vue(), tailwindcss()
+  ],
+    server: {
     proxy: {
       "/api": {
         target: process.env.VITE_API_TARGET || "http://127.0.0.1:3001",
